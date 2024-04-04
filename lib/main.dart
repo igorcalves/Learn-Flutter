edd34edb9a9a014a874c50fae34f2d3c97cb8cb0
Igor Alves Linux <igor193@live.com>
2024-04-04 17:05:44 -0300

    t

--- lib/main.dart
+++ lib/main.dart
@@ -8,8 +8,11 @@ import 'package:learn/repository/user_repository.dart';
 
 void main() {
   RepositoryImplementing fake = FakeRepository(MyRepo.getData());
+
   RepositoryImplementing windowsLocal = UserRepository('http://192.168.56.1:8080');
+
   RepositoryImplementing linuxLocal = UserRepository('http://192.168.0.90:8080');
+  
   RepositoryImplementing gitpod = UserRepository('https://8080-igorcalves-condominusba-8hnd04t45m2.ws-us110.gitpod.io');
   
 
